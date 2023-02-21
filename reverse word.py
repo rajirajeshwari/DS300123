07d81460f47603115a723d35ce2f6da7deda300e
rajirajeshwari <124510880+rajirajeshwari@users.noreply.github.com>
2023-02-21 07:30:24 +0530

    Add files via upload

---
+++ reverse word.py
@@ -0,0 +1,11 @@
+#word = input("Enter a word: ")
+#reversed_word = word[::-1]
+#print("Reverse word : ", reversed_word)
+
+word = input("Enter a word: ")
+reversed_word = ""
+
+for i in range(len(word)-1, -1, -1):
+    reversed_word += word[i]
+
+print("Reversed word:", reversed_word)
