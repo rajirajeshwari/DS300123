7e49772be6a7ab5f20212385c6366bb2822f8c87
rajirajeshwari <124510880+rajirajeshwari@users.noreply.github.com>
2023-03-24 19:55:06 +0530

    Add files via upload

---
+++ Assignment-5.py
@@ -0,0 +1,11 @@
+class Point:
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+    
+    def square(self):
+        return self.x**2 + self.y**2 + self.z**2
+p = Point(1, 3, 5)
+print(p.square())
+
